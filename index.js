4cfd786c10df554d8fdd3d24eb8bc497a14145c7
Seabass <beattycharles@yahoo.com>
2022-12-09 18:27:47 -0500

    need to generate readme

--- index.js
+++ index.js
@@ -3,19 +3,77 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-// Title, Sections for Description, TOC, Installation, Usage
+// Title, Sections for Description, Table Of Content, Installation, Usage
 //License, Contributin, Test, Questions
-const questions = [
-
-];
+const questions = () => {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: 'Title',
+        message: 'Whats the title of the project?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: "Give a description of the project.",
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: "Requirerment need for the app.",
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: "What are the usage instrctions for this app.",
+      },
+      {
+        type: 'list',
+        name: 'License',
+        message: 'What license are you useing?',
+        choices: ['MIT', 'GNU General PUblic License v3.0', 'Apache', 'Mozilla Public License 2.0'],
+      },
+      {
+        type: 'input',
+        name: 'Contributing',
+        message: "Any Contributers?"
+      },
+      {
+        type: 'input',
+        name: 'Tests',
+        message: "Any testing instructions for the user?"
+      },
+      {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your Github username?'
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email address?'
+      }
+    ])
+    .then((answers) => {
+        const writetoFile = {    
+        fs.writeFile('./New/README.md', questions, (err) =>
+          err ? console.log(err) : console.log('Successfully created README.md in New folder!!')
+        )
+    }
+      }
+      )
+    };
+    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile("README.md,")
 
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(questions) {}
 
 // Function call to initialize app
 init();
@@ -26,31 +84,6 @@ inquirer
       type: 'input',
       name: 'name',
       message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
     },
   ])
   .then((answers) => {
